47eea83d500fa3e0666712c876a1728e37639ac5
Abhishek <abhishekparmar162@gmail.com>
2022-12-04 18:56:58 +0530

    added env local

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify";
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET
+    })
+  ]
+  ,
+}
+export default NextAuth(authOptions)
