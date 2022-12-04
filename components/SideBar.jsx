23b3a9100dcc3f1a647f42b886fbaa7540a4887f
Abhishek <abhishekparmar162@gmail.com>
2022-12-04 12:08:27 +0530

    added styling

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome,AiOutlineSearch,AiOutlinePlayCircle ,AiTwotoneHeart} from "react-icons/ai";
+import {HiLibrary} from 'react-icons/hi' ;
+import {GrSpotify} from 'react-icons/gr' ;
+
 
 function SideBar() {
   return (
@@ -9,23 +12,23 @@ function SideBar() {
         <h1 className="pl-2 font-semibold text-gray-400">Home</h1>
       </div>
       <div className="flex item-center p-2">
-        <AiOutlineHome size={20} />
-        <h1 className="pl-2 font-semibold text-gray-400">Search</h1>
+        <AiOutlineSearch size={25} />
+        <h1 className="pl-1 font-semibold text-gray-400">Search</h1>
       </div>
       <div className="flex item-center p-2">
-        <AiOutlineHome size={20} />
+        <HiLibrary size={20} />
         <h1 className="pl-2 font-semibold text-gray-400">Your Library</h1>
       </div>
-      <div className="flex item-center p-2">
-        <AiOutlineHome size={20} />
+      <div className="flex item-center p-2 mt-4">
+        <AiOutlinePlayCircle size={20} />
         <h1 className="pl-2 font-semibold text-gray-400">Created Playlist</h1>
       </div>
       <div className="flex item-center p-2">
-        <AiOutlineHome size={20} />
-        <h1 className="pl-2 font-semibold text-gray-400">Liked Songs</h1>
+        <AiTwotoneHeart size={20}  className='text-blue-500'/>
+        <h1 className="pl-2 font-semibold  text-gray-400">Liked Songs</h1>
       </div>
       <div className="flex item-center p-2">
-        <AiOutlineHome size={20} />
+        <GrSpotify size={20} className='text-green-400' />
         <h1 className="pl-2 font-semibold text-gray-400">Your Episodes</h1>
       </div>
     </div>
