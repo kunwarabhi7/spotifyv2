0d228d5acc77f10c1171db29acd4722e9e6c04e2
Abhishek <abhishekparmar162@gmail.com>
2022-12-06 16:43:53 +0530

    added basic styling

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,11 +1,17 @@
 import {getProviders,signIn} from 'next-auth/react'
-
+import {BsSpotify} from 'react-icons/bs'
 
 const login = ({providers}) => {
   return (
-    <div>
+    <div className='mt-20'>
+      <div className='flex justify-center '>
+      <BsSpotify size={40} />
+      <h1 className='text-3xl font-bold'>Spotify</h1>
+      
+    </div>
+      <hr className='text-black my-16 bg-black  text-4xl'/>
 {Object.values(providers).map((provider)=>(
-  <div>Login With {provider.name}</div>
+  <div className='text-center text-4xl'>Login With {provider.name}</div>
 ))}
     </div>
   )
