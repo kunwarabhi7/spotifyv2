0c4ade578bf6bbfcf2d1e13203e81d537c68cec5
Abhishek <abhishekparmar162@gmail.com>
2022-12-16 21:21:40 +0530

    added header

--- components/MainBar.jsx
+++ components/MainBar.jsx
@@ -9,11 +9,15 @@ const MainBar = () => {
   return (
     <div className='w-full h-screen'>
 {/* Header */}
-<div className='bg-black h-[418px] text-white'>
-  <div className='flex p-6'>
+<div className='bg-black h-[418px] text-white flex justify-between p-6'>
+  <div className='flex '>
 
 <AiOutlineLeft size={30} className='mr-4 rounded-full '/>
 <AiOutlineRight size={30} className='mr-4 rounded-full '/>
+  </div>
+  <div className='flex '>
+    <div className='mr-4'>Upgrade</div>
+    <div>Name</div>
   </div>
 </div>
         </div>
