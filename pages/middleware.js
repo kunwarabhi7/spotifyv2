710413bbcd69aeec5a0d87c6a39eeda637b882b3
Abhishek <abhishekparmar162@gmail.com>
2022-12-16 17:17:38 +0530

    added middleware

---
+++ pages/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(req) {
+const token = await getToken({req,secret:process.env.JWT_SECRET}) 
+
+const {pathname} =  req.nextUrl
+
+if(pathname.includes('api/auth')|| token){
+  return NextResponse.next();
+}
+if(!token && pathname !=='/login'){
+  return NextResponse.redirect('/login')
+}
+}
+
+
