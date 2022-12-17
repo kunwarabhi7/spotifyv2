5c81a142b0a53227af3f500e4358808746b46a31
Abhishek <abhishekparmar162@gmail.com>
2022-12-17 15:53:13 +0530

    fix styling

--- components/MainBar.jsx
+++ components/MainBar.jsx
@@ -10,21 +10,21 @@ const MainBar = () => {
   return (
     <div className='w-full h-screen'>
 {/* Header */}
-<div className='bg-black h-[418px] text-white flex justify-between p-6'>
+<div className='bg-gradient-to-b from-black to-red-500   h-[418px] text-white flex justify-between p-6'>
   <div className='flex h-8 '>
 
 <AiOutlineLeft size={30} className='mr-4 rounded-full '/>
 <AiOutlineRight size={30} className='mr-4 rounded-full '/>
   </div>
   <div className='flex '>
-    <div className='mr-8'>
+    <div className='mr-4'>
       <p className='border  font-bold rounded-full p-2 py-1'>
 
       Upgrade
       </p>
       </div>
-    <div className='flex justify-center hover:opacity-80 '>
-      <img className='rounded-full w-12 h-12 mr-2 cursor-pointer' src={session?.user?.image} />
+    <div className='flex justify-center hover:opacity-80 rounded-full  border h-8 '>
+      <img className='rounded-full  mr-2 cursor-pointer' src={session?.user?.image} />
       <h1 className='cursor-pointer'>{session?.user?.name}</h1>
       <AiFillCaretDown className='cursor-pointer' size={25}/>
     </div>
