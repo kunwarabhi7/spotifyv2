61c6acef46c236c18a4561d9e1a39af868a42e2e
Abhishek <abhishekparmar162@gmail.com>
2023-01-06 17:17:53 +0530

    added UI

--- components/Songs.jsx
+++ components/Songs.jsx
@@ -1,19 +1,26 @@
 import { useRecoilValue } from "recoil"
 import {  playlistState } from "../atoms/playlistAtom"
+import {CiClock2 } from 'react-icons/ci'
+import Song from "./Song"
 
 
 const Songs = () => {
     const playlist = useRecoilValue(playlistState)
   return (
     <div>
-        <h1>Songs</h1>
+        <div  className="flex text-white my-4 ">
+        <h1 className="ml-16"># Title</h1>
+        <h1 className="ml-16">ALBUM</h1>
+        <h1 className="ml-16">DATE ADDED</h1>
+        <CiClock2 className="ml-16" size={25} />
+        
+    </div>
         <h1 className="text-white bg-black">
 
 {playlist?.tracks?.items.map((playlistt)=>{
     return(
 <div>
-    {playlistt.track.name}
-
+<Song key={playlistt.track.id} track={playlistt}  />
 </div>
         )
 })}
