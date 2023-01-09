3118f08e909d1208e7f0fe47ff8197626615ade9
Abhishek <abhishekparmar162@gmail.com>
2023-01-09 18:16:24 +0530

    fix stylingg

--- components/Song.jsx
+++ components/Song.jsx
@@ -11,15 +11,15 @@ const ReadableDate = date.toLocaleDateString({
 
   return (
     <div className='flex'>
-      <p className='m-2'>{order+1}</p>
+      <p className='m-2 '>{order+1}</p>
       <img className='w-14 h-12 m-4' src={track.track.album.images[0].url} />
       <div>
       <h1 className='text-white text-xl mt-3 ml-1 mr-2 w-36 lg:w-64 truncate'>{track?.track?.name}</h1>
-      <h1 className='text-xs font-thin ml-2 text-gray-400 '>{track.track.artists[0].name}</h1>
+      <h1 className='text-xs font-thin ml-2 text-gray-500 '>{track.track.artists[0].name}</h1>
       </div>
-      <h1 className='text-xs text-gray-400 mt-4 ml-4'>{track.track.album.name}</h1>
-      <h1>{ReadableDate}</h1>
-      <h1 className='ml-4'>{time(track.track.duration_ms)}</h1>
+      <h1 className=' text-gray-400 mt-4 w-36 lg:w-64 ml-4'>{track.track.album.name}</h1>
+      <h1 className='w-32 lg:w-64 ml-32'>{ReadableDate}</h1>
+      <h1 className='ml-64 w-36 lg:w-64 '>{time(track.track.duration_ms)}</h1>
     </div>
   )
 }
