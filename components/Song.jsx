c14bff1aac00944ab60744d4b5591f81ac5c0e77
Abhishek <abhishekparmar162@gmail.com>
2023-01-09 16:02:01 +0530

    added artist name

--- components/Song.jsx
+++ components/Song.jsx
@@ -4,9 +4,12 @@ const Song = ({order,track}) => {
 
   return (
     <div className='flex'>
-      <p>{order+1}</p>
-      <img className='w-80 h-60' src={track.track.album.images[0].url} />
-      <h1 className='text-white'>{track?.track?.name}</h1>
+      <p className='m-2'>{order+1}</p>
+      <img className='w-14 h-12 m-4' src={track.track.album.images[0].url} />
+      <div>
+      <h1 className='text-white text-xl mt-3 ml-1'>{track?.track?.name}</h1>
+      <h1 className='text-xs font-thin ml-2 text-gray-400 '>{track.track.artists[0].name}</h1>
+      </div>
     </div>
   )
 }
