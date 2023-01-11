9e74ab9ff52997ead1304a85073737b3b871fbbf
Abhishek <abhishekparmar162@gmail.com>
2023-01-11 21:27:55 +0530

    fix css

--- components/Song.jsx
+++ components/Song.jsx
@@ -15,7 +15,7 @@ const ReadableDate = date.toLocaleDateString({
       <img className='w-14 h-12 m-4' src={track.track.album.images[0].url} />
       <div>
       <h1 className='text-white text-xl mt-3 ml-1 mr-2 w-36 lg:w-64 truncate'>{track?.track?.name}</h1>
-      <h1 className='text-xs font-thin ml-2 text-gray-500 '>{track.track.artists[0].name}</h1>
+      <h1 className='text-xs  ml-2 text-gray-400 '>{track.track.artists[0].name}</h1>
       </div>
       <h1 className=' text-gray-400 mt-4 w-36 lg:w-64 ml-4'>{track.track.album.name}</h1>
       <h1 className='w-32 lg:w-64 ml-32'>{ReadableDate}</h1>
