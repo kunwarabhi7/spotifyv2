1ed36e15dac105a37f1a8a09cbb7f6112dfe0dd6
Abhishek <abhishekparmar162@gmail.com>
2023-01-18 17:42:14 +0530

    fix css

--- components/Player.jsx
+++ components/Player.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Player = () => {
   return (
     <div className='sticky bottom-0'>
-      Playerrrrrr
+      Playerrrrrrrrr
     </div>
   )
 }
 
 export default Player
+ 
